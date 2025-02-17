c6027118d29786dab6fcdc7282f18c641bfaba54
davi santana <davzn15@gmail.com>
2025-02-17 16:25:33 -0300

    feat: adição da tarefa 2 com pequenas mudanças no html e no css para melhor funcionamento

--- todolist.js
+++ todolist.js
@@ -1,21 +1,34 @@
-
 function adicionarTarefa() {
-         
     var tarefaTexto = document.getElementById('tarefaInput').value;
-    
 
     if (tarefaTexto.trim() !== '') {
-       
+      
         var novaTarefa = document.createElement('li');
         
-     
-        novaTarefa.textContent = tarefaTexto;
         
+        novaTarefa.innerText = tarefaTexto;
+
       
+        var botaoRemover = document.createElement('button');
+        botaoRemover.innerText = 'Remover';
+
+        
+        botaoRemover.addEventListener('click', function() {
+            novaTarefa.remove();
+        });
+
+      
+        novaTarefa.appendChild(botaoRemover);
+        
+       
         document.getElementById('listaTarefas').appendChild(novaTarefa);
+        
         
         document.getElementById('tarefaInput').value = '';
     } else {
         alert('Por favor, insira uma tarefa.');
     }
 }
+
+
+

36d1fc8c99d2f8bbb41d38fdb6f6596c09a204ce
Davi de Jesus Silva Santana <davi.santana@renova.net.br>
2025-02-18 16:32:39 -0300

    feat: adição da terceira tarefa

--- todolist.js
+++ todolist.js
@@ -1,29 +1,53 @@
 function adicionarTarefa() {
     var tarefaTexto = document.getElementById('tarefaInput').value;
+    let jaClicou;
 
     if (tarefaTexto.trim() !== '') {
-      
+
         var novaTarefa = document.createElement('li');
-        
-        
+
+
         novaTarefa.innerText = tarefaTexto;
 
-      
+
         var botaoRemover = document.createElement('button');
         botaoRemover.innerText = 'Remover';
 
-        
-        botaoRemover.addEventListener('click', function() {
+
+        botaoRemover.addEventListener('click', function () {
             novaTarefa.remove();
         });
 
-      
+        var botaoConcluir = document.createElement('button');
+        botaoConcluir.innerText = 'Concluída';
+
+
+        botaoConcluir.addEventListener('click', function () {
+           
+         
+            novaTarefa.style.backgroundColor = "#05ff5d";
+            novaTarefa.style.color = "black";
+
+            if (jaClicou) {
+                novaTarefa.style.backgroundColor = "black";
+                novaTarefa.style.color = "#05cdff";
+                jaClicou = false
+                botaoConcluir.innerText = 'Concluída';
+                return
+
+            }
+            jaClicou = true;
+            botaoConcluir.innerText = 'Não concluída';
+            
+        });
+
+
         novaTarefa.appendChild(botaoRemover);
-        
-       
+        novaTarefa.appendChild(botaoConcluir);
+
         document.getElementById('listaTarefas').appendChild(novaTarefa);
-        
-        
+
+
         document.getElementById('tarefaInput').value = '';
     } else {
         alert('Por favor, insira uma tarefa.');
